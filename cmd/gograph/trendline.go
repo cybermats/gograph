3e838de3e796a0b2138df7f690badf6f095e34f4
Mats Fredriksson <cybermats@gmail.com>
2019-10-27 19:53:29 +0100

    Narrowed the type for Trendline. Doesn't have to be an unbounded slice.

--- cmd/gograph/trendline.go
+++ cmd/gograph/trendline.go
@@ -1,8 +1,8 @@
 package main
 
 type trendline struct {
-	X []int
-	Y []float32
+	X [2]int
+	Y [2]float32
 }
 
 type trendlineMaker struct {
@@ -23,12 +23,12 @@ func (tm *trendlineMaker) addSample(x, y float32) {
 
 func (tm *trendlineMaker) trendline(offset int) trendline {
 	if tm.n == 0 || tm.sumX == 0 {
-		return trendline{[]int{0, 0}, []float32{0, 0}}
+		return trendline{[2]int{0, 0}, [2]float32{0, 0}}
 	}
 	alpha := (float32(tm.n)*tm.sumXY - tm.sumX*tm.sumY) /
 		(float32(tm.n)*tm.sumXX - tm.sumX*tm.sumX)
 	beta := (tm.sumY - alpha*tm.sumX) / float32(tm.n)
-	xaxis := []int{offset, offset + tm.n - 1}
-	yaxis := []float32{alpha*float32(offset) + beta, alpha*float32(offset+tm.n-1) + beta}
+	xaxis := [2]int{offset, offset + tm.n - 1}
+	yaxis := [2]float32{alpha*float32(offset) + beta, alpha*float32(offset+tm.n-1) + beta}
 	return trendline{xaxis, yaxis}
 }
