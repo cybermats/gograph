1d767e11f816ffb23f5b99ca9ac0730316beb2f8
Mats Fredriksson <cybermats@gmail.com>
2019-10-27 19:55:18 +0100

    Fixed stable sort if values are the same. It now sorts by keys.

--- internal/helper/sortmapbyvalue.go
+++ internal/helper/sortmapbyvalue.go
@@ -23,6 +23,11 @@ type Pair struct {
 // PairList represents a sorted map
 type PairList []Pair
 
-func (p PairList) Len() int           { return len(p) }
-func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
-func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Len() int { return len(p) }
+func (p PairList) Less(i, j int) bool {
+	if p[i].Value == p[j].Value {
+		return p[i].Key < p[j].Key
+	}
+	return p[i].Value < p[j].Value
+}
+func (p PairList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
